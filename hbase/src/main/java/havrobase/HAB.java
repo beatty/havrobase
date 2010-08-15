9f2d0fa2f7fd01dd76bcc7fe89d7c44103197dc3
John Beatty <john.beatty@gmail.com>
2010-08-14 22:02:28 -0700

    oops, set back to LZO -- had changed for local dev purposes

--- hbase/src/main/java/havrobase/HAB.java
+++ hbase/src/main/java/havrobase/HAB.java
@@ -160,7 +160,7 @@ public class HAB<T extends SpecificRecord> extends SolrAvroBase<T, byte[]> {
     HTableInterface schemaTable;
     HColumnDescriptor family = new HColumnDescriptor(AVRO_FAMILY);
     family.setMaxVersions(1);
-    family.setCompressionType(Compression.Algorithm.NONE);
+    family.setCompressionType(Compression.Algorithm.LZO);
     family.setInMemory(true);
     HTableDescriptor tableDesc = new HTableDescriptor(schemaName);
     tableDesc.addFamily(family);
@@ -528,7 +528,7 @@ public class HAB<T extends SpecificRecord> extends SolrAvroBase<T, byte[]> {
   private HColumnDescriptor getColumnDesc(byte[] columnFamily) {
     HColumnDescriptor family = new HColumnDescriptor(columnFamily);
     family.setMaxVersions(1);
-    family.setCompressionType(Compression.Algorithm.NONE);
+    family.setCompressionType(Compression.Algorithm.LZO);
     family.setInMemory(false);
     return family;
   }
